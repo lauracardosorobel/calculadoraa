c691c5cd59dde03f723c4267dd6ed5b843ca9357
lauracardosorobel <113368682+lauracardosorobel@users.noreply.github.com>
2023-11-29 14:28:39 -0300

    Add files via upload

--- tabuada.js
+++ tabuada.js
@@ -1,14 +1,14 @@
-const frm = document.querySelector("form")
-const resp = document.querySelector("pre")
+const form= document.querySelector("form")
+const resp= document.querySelector("pre")
 
-frm.addEventListener("submit", (e) =>{
+form.addEventListener("submit", (e) =>{
     e.preventDefault()
 
-    const numero = Number(frm.inNumero.value)
-    let resposta = ""
-    for(let i = 1; i <= 10; i++){
-        resposta = resposta + numero + " x " + i + " = " + (numero * i) + "\n"
+    const numero= Number(form.inNumero.value)
+    let resposta= ""
+    for(let i=1; i <=10; i++){
+        resposta= resposta + numero + " x " + i + " = " + (numero * i) + "\n"
     }
     resp.innerText = resposta
-
+    
 })
